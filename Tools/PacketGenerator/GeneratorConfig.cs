a843fd8420160344af7821ebddaf8f84c14c2692
Jaeseok <jaeseok1111@gmail.com>
2020-09-10 13:29:48 +0900

    Packet Generator 프로젝트 추가
    - 서버 프로젝트 위치 수정

---
+++ Tools/PacketGenerator/GeneratorConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PacketGenerator
+{
+    public static class GeneratorConfig
+    {
+        #region Properties
+        public static String DefinitionPath { get; private set; }
+        public static String WorkingDirectory { get; private set; }
+        public static String OutputDirecotry { get; private set; }
+        #endregion
+
+        #region Methods
+        public static void Init(String[] args)
+        {
+            if (args.Length < 2)
+            {
+                throw ThrowArgumentException();
+            }
+
+            WorkingDirectory = Environment.CurrentDirectory;
+
+            foreach (String argument in args)
+            {
+                if (argument.Contains("--workingPath="))
+                {
+                    WorkingDirectory = argument.Replace("--workingPath=", "");
+                }
+                else if (argument.Contains("--definitionPath="))
+                {
+                    DefinitionPath = argument.Replace("--definitionPath=", "");
+                }
+                else if (argument.Contains("--outputPath="))
+                {
+                    OutputDirecotry = argument.Replace("--outputPath=", "");
+                }
+                else
+                {
+                    throw ThrowArgumentException();
+                }
+            }
+
+            if (String.IsNullOrWhiteSpace(DefinitionPath) || String.IsNullOrWhiteSpace(OutputDirecotry))
+            {
+                throw ThrowArgumentException();
+            }
+        }
+        #endregion
+
+        #region Private
+        private static Exception ThrowArgumentException()
+        {
+            return new Exception(@"PacketGenerator: Invalid Argument
+                --workingPath: Working Directory (optional)
+                --definitionPath: Packet Definition Path (.xml)
+                --outputPath: Packet Code Output Directory
+
+                Example: PacketGenerator.exe --definitionPath=DenifitionDirectory/PacketDefinition.xml --outputPath=OutputDirectory
+            ");
+        }
+        #endregion
+    }
+}
---
+++ Tools/PacketGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PacketGenerator
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                GeneratorConfig.Init(args);
+
+                Console.WriteLine($"Packet Denifition: {GeneratorConfig.DefinitionPath}");
+                Console.WriteLine($"Output Directory: {GeneratorConfig.OutputDirecotry}");
+                Console.WriteLine($"Working Directory: {GeneratorConfig.WorkingDirectory}");
+                Console.WriteLine($"Starting Packet Code Generator...");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+    }
+}
