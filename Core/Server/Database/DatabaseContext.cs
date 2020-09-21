b6542d0342803ed23d684447c1a597fc3bf574ce
jaeseok1111 <jaeseok1111@gmail.com>
2020-09-22 04:51:46 +0900

    MySql 추가

---
+++ Core/Server/Database/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Core.Server.Database
+{
+    public class DatabaseContext : IDisposable
+    {
+        #region Properties
+        private MySqlConnection mSqlConnection;
+        #endregion
+
+        #region Methods
+        public DatabaseContext(String connectionString)
+        {
+            mSqlConnection = new MySqlConnection(connectionString);
+            mSqlConnection.Open();
+        }
+
+        public void Dispose()
+        {
+            mSqlConnection.Close();
+            mSqlConnection.Dispose();
+        }
+        #endregion
+    }
+}
