23d8ea67d73cdf3059e11ea48807cf13ab78f7c0
jaeseok1111 <jaeseok1111@gmail.com>
2020-09-24 05:26:54 +0900

    DatabaseContext 수정 / DatabaseContextPool 추가

--- Core/Server/Database/DatabaseContext.cs
+++ Core/Server/Database/DatabaseContext.cs
@@ -7,6 +7,7 @@ namespace Core.Server.Database
     {
         #region Properties
         private MySqlConnection mSqlConnection;
+        private MySqlCommand mCommand;
         #endregion
 
         #region Methods
@@ -16,10 +17,37 @@ namespace Core.Server.Database
             mSqlConnection.Open();
         }
 
+        public void ExecuteQuery(String query, params MySqlParameter[] parameters)
+        {
+            CreateCommand(query, parameters).ExecuteNonQuery();
+        }
+
+        public MySqlDataReader ExecuteReader(String query, params MySqlParameter[] parameters)
+        {
+            return CreateCommand(query, parameters).ExecuteReader();
+        }
+
         public void Dispose()
         {
             mSqlConnection.Close();
             mSqlConnection.Dispose();
+        }
+        #endregion
+
+        #region Private
+        private MySqlCommand CreateCommand(String query, params MySqlParameter[] parameters)
+        {
+            if (mCommand == null)
+            {
+                mCommand = new MySqlCommand(query, mSqlConnection);
+            }
+
+            mCommand.CommandText = query;
+            
+            mCommand.Parameters.Clear();
+            mCommand.Parameters.AddRange(parameters);
+
+            return mCommand;
         }
         #endregion
     }
---
+++ Core/Server/Database/DatabaseContextPool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Server.Database
+{
+    public class DatabaseContextPool : IDisposable
+    {
+        #region Properties
+        private Stack<DatabaseContext> mContexts;
+        private String mConnectionString;
+        #endregion
+
+        #region Methods
+        public DatabaseContextPool(String connectionString)
+        {
+            mContexts = new Stack<DatabaseContext>();
+            mConnectionString = connectionString;
+        }
+
+        public void Push(DatabaseContext context)
+        {
+            mContexts.Push(context);
+        }
+
+        public DatabaseContext Pop()
+        {
+            if (mContexts.Count == 0)
+            {
+                return new DatabaseContext(mConnectionString);
+            }
+
+            return mContexts.Pop();
+        }
+
+        public void Dispose()
+        {
+            foreach (DatabaseContext context in mContexts)
+            {
+                context.Dispose();
+            }
+        }
+        #endregion
+    }
+}
